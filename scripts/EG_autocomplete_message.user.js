1864de67436d0a5002c16a2ec4e79351852c0d3e
Alex <alexander.v.schmalz@gmail.com>
2017-08-08 21:59:03 +0200

    Update EG_autocomplete_message.user.js
    Added extern library "awesomecomplete"
    Changed whole behavior to match new awesomecomplete functionality
    
    localstorage not yet implemented!

--- scripts/EG_autocomplete_message.user.js
+++ scripts/EG_autocomplete_message.user.js
@@ -2,87 +2,102 @@
 // @name         EG_autocomplete_message
 // @namespace    http://tampermonkey.net/
 // @version      0.0.1
-// @description  Autovervollständigung
+// @description  Autovervollstaendigung
 // @author       Tenzo & Nojheim
+// @require      http://www.versi.info/EG/awesomecomplete/awesomplete.min.js
+// @resource     awesomeCSS http://www.versi.info/EG/awesomecomplete/awesomplete.css
 // @include      https://zyrthania.evergore.de/evergore.html?page=msg_new*
 // @include      https://zyrthania.evergore.de/evergore.html?page=retail_new*
 // @grant        GM_addStyle
+// @grant       GM_getResourceText
 // ==/UserScript==
 
 (function () {
-    'use strict';
+	'use strict';
 
-    GM_addStyle('* input::-webkit-calendar-picker-indicator { display: none;}');
+	var newCSS = GM_getResourceText("awesomeCSS");
+	GM_addStyle(newCSS);
+	GM_addStyle('* input::-webkit-calendar-picker-indicator { display: none;}');
 
-    var map = [];
-    var mapSizeChanged = new Event('sizeChanged');
+	//Setup globals
+	var map = [];
+	var mapSizeChanged = new Event('sizeChanged');
+	var awesomeplete;
+	//Datalist structure
+	var list = document.createElement("DATALIST");
+	list.setAttribute("id", "nameList");
 
-    addAutocompleteBehavior();
+	//add Autocomplete functionality
+	addAutocompleteBehavior();
 
-    function addAutocompleteBehavior() {
-        var inputfield;
+	function addAutocompleteBehavior() {
+		var inputfield;
 
-        if (document.getElementsByName('recipient')[0] == null) {
-            inputfield = "target";
-        } else {
-            inputfield = "recipient";
-        }
+		if (document.getElementsByName('recipient')[0] == null) {
+			inputfield = "target";
+		} else {
+			inputfield = "recipient";
+		}
 
-        var list = document.createElement("DATALIST");
-        list.setAttribute("id", "nameList");
-        document.getElementsByName(inputfield)[0].appendChild(list);
+		document.getElementsByName(inputfield)[0].setAttribute("list", "nameList");
+		document.getElementsByName(inputfield)[0].setAttribute("class", "awesomplete");
+		loadPages();
+		addDatalistToPage();
 
-        var z1 = document.createElement("OPTION");
-        z1.setAttribute("value", "Chrome");
-        document.getElementById("nameList").appendChild(z1);
+	}
 
-        document.getElementsByName(inputfield)[0].setAttribute("list", "nameList");
-        loadPages();
-    }
+	function addDatalistToPage() {
+		document.getElementsByClassName("awesomplete")[0].append(list);
+		//focus on input field
+		document.getElementsByClassName("awesomplete")[0].focus();
 
-    function loadPages(){
-        var elementCount = 1000;
-        var lastPageNumber = Math.ceil(elementCount/20);
+	}
 
-        document.addEventListener('sizeChanged', function (e) {
-            var mapSize = Object.keys(map).length;
-            if (mapSize == lastPageNumber - 1) {
-                var nameList = document.getElementById("nameList");
-                for (var j = 0; j < lastPageNumber-1; j++) {
-                    var rowsToAppend = map[j];
-                    for (var i = 1; i < rowsToAppend.length; i++) {
-                        var optionElement = document.createElement("OPTION");
-                        optionElement.setAttribute("value", rowsToAppend[i].getElementsByTagName('a')[0].innerHTML);
-                        nameList.appendChild(optionElement);
-                    }
-                }
-            }
-        }, false);
+	function loadPages() {
+		console.log("loadPages");
+		var elementCount = 300;
+		var lastPageNumber = Math.ceil(elementCount / 20);
 
-        for (var i = 11; i < elementCount; i+=20) {
-            loadXMLDoc('https://zyrthania.evergore.de/evergore.html?page=ranking_hero&pos='+i);
-        }
-    }
+		//event is fired as soon 'loadXMLDoc()' finishes loading a website
+		document.addEventListener('sizeChanged', function (e) {
+			var mapSize = Object.keys(map).length;
+			if (mapSize == lastPageNumber - 1) {
+				for (var j = 0; j < lastPageNumber - 1; j++) {
+					var rowsToAppend = map[j];
+					for (var i = 1; i < rowsToAppend.length; i++) {
+						var optionElement = document.createElement("OPTION");
+						optionElement.innerHTML = rowsToAppend[i].getElementsByTagName('a')[0].innerHTML;
+						list.appendChild(optionElement);
+					}
+				}
+			}
+		}, false);
 
-    function loadXMLDoc(theURL) {
-        var xmlhttp;
-        if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari, SeaMonkey
-            xmlhttp = new XMLHttpRequest();
-        } else { // code for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onload = function () {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                var doc = document.implementation.createDocument('', 'html', null);
-                var body = document.createElementNS('', 'body');
-                body.innerHTML = xmlhttp.responseText;
-                doc.documentElement.appendChild(body);
-                var rowsToAppend = doc.getElementById('CONT_BODY').getElementsByTagName("table")[2].getElementsByTagName("tr");
-                map.push(rowsToAppend);
-                document.dispatchEvent(mapSizeChanged);
-            }
-        };
-        xmlhttp.open("GET", theURL, true);
-        xmlhttp.send();
-    }
+		for (var i = 11; i < elementCount; i += 20) {
+			loadXMLDoc('https://zyrthania.evergore.de/evergore.html?page=ranking_hero&pos=' + i);
+		}
+	}
+
+	function loadXMLDoc(theURL) {
+		console.log("loadXMLDoc");
+		var xmlhttp;
+		if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari, SeaMonkey
+			xmlhttp = new XMLHttpRequest();
+		} else { // code for IE6, IE5
+			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xmlhttp.onload = function () {
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				var doc = document.implementation.createDocument('', 'html', null);
+				var body = document.createElementNS('', 'body');
+				body.innerHTML = xmlhttp.responseText;
+				doc.documentElement.appendChild(body);
+				var rowsToAppend = doc.getElementById('CONT_BODY').getElementsByTagName("table")[2].getElementsByTagName("tr");
+				map.push(rowsToAppend);
+				document.dispatchEvent(mapSizeChanged);
+			}
+		};
+		xmlhttp.open("GET", theURL, true);
+		xmlhttp.send();
+	}
 })();
