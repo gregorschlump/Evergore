4d8d5fd0ec30e7b16f4cdee6a5108f523e444cf2
Alex <alexander.v.schmalz@gmail.com>
2018-10-31 00:50:40 +0100

    Update EG_protocol_one_page.user.js

--- scripts/EG_protocol_one_page.user.js
+++ scripts/EG_protocol_one_page.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EG_protocol_one_page
 // @namespace    http://tampermonkey.net/
-// @version      0.0.6
+// @version      0.0.7
 // @description  Läd alle Einträge der Protokolle
 // @author       Nojheim
 // @include        http://zyrthania.evergore.de/evergore.html?page=guild_protocol*
@@ -83,7 +83,7 @@
             }
         };
         xmlhttp.open("GET", theURL, true);
-	xmlhttp.overrideMimeType('text/xml; charset=iso-8859-1');
+	xmlhttp.overrideMimeType('text/xml; charset=utf-8');
         xmlhttp.send();
     }
 })();
